7839cf656a4b1dfbb63301d8605776ec3d396b7e
Gabriel Lobo <globo.coding@gmail.com>
2022-11-24 02:25:33 -0600

    Updated Header with Interfaces

--- src/layout/basicLayout.tsx
+++ src/layout/basicLayout.tsx
@@ -1,9 +1,24 @@
-import { Header } from '../components/Header/Header';
+import Header from '../components/Header/Header';
+import { HeaderLink } from '../components/Header/Header';
+import { useTranslation } from 'react-i18next';
 
 export const withBasicLayout = (Children: () => JSX.Element) => () => {
+  const { t } = useTranslation('header');
+
+  const headerLinks: HeaderLink[] = [
+    {
+      label: `${t('home')}`,
+      link: '/',
+    },
+  ];
   return (
     <div className='flex min-w-screen min-h-screen'>
-      <Header />
+      <Header
+        links={headerLinks}
+        hideGitHubLink={true}
+        alignRight={true}
+        isFat={true}
+      />
       <main className='w-screen'>
         <Children />
       </main>

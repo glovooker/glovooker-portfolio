d6cb5f5782b1c6ae85f019614575d8de8513cb7e
Gabriel Lobo <globo.coding@gmail.com>
2022-11-23 19:28:38 -0600

    Routes with Basic Functionality Finished

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Routing from './Routing';
+
+export default function App() {
+  return <Routing />;
+}
---
+++ src/Routing.tsx
@@ -0,0 +1,40 @@
+import React, { LazyExoticComponent, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export type Pages = {
+  [key: string]: {
+    component: LazyExoticComponent<() => JSX.Element>;
+    path: string;
+  };
+};
+
+export const pages: Pages = {
+  home: {
+    component: React.lazy(() => import('./pages/Home')),
+    path: '/home',
+  },
+};
+
+const Routing = () => {
+  const pagesKeys = Object.keys(pages || {});
+  return (
+    <Routes>
+      {pagesKeys.map((pageKey) => {
+        const { path, component: Component } = pages[pageKey];
+        return (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Component />
+              </Suspense>
+            }
+          />
+        );
+      })}
+    </Routes>
+  );
+};
+
+export default Routing;
